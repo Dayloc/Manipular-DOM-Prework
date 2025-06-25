ede3e16b721c0d644b6e45e5560f136947ec3041
Dayloc Yumar <daylocyumar@gmail.com>
2025-06-25 17:17:53 +0200

    Agregando el windowOnload

--- javaScript.js
+++ javaScript.js
@@ -55,7 +55,31 @@ button.addEventListener('click', () => {
 
   // Extra: crear otro elemento al hacer click
   const otro = document.createElement('div');
+
   otro.textContent = "¡Nuevo div por click!";
+  
   otro.classList.add('nuevo');
+  
   document.body.appendChild(otro);
 });
+
+/* window.onload = function () {
+ 
+
+  let excuse = document.getElementById("excusas");
+  
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+for(let i = 0; i < 5; i++){
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  excusas.innerHTML = who[randomWho] + " " + action[randomAction] + " " + what[randomWhat] + " " + when[randomWhen];
+ 
+ }
+}; */
